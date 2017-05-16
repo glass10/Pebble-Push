355cf27b6135ca0c7e33483446e41b7ca79cae21
Cameron Glass <glass10@purdue.edu>
2017-05-15 22:59:42 -0400

    Update Lamda Code

--- index.js
+++ index.js
@@ -27,26 +27,94 @@ exports.handler = (event, context) => {
         console.log(`INTENT REQUEST`)
 
         switch(event.request.intent.name) {
-          case "normalReq":
+         case "normalReq":
+            var reminder = event.request.intent.slots.Text.value;
+            var date = event.request.intent.slots.Date.value;
+            var time = event.request.intent.slots.Time.value;
+
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth()+1; //January is 0!
+            var yyyy = today.getFullYear();
+
+            if(dd<10) {
+                dd='0'+dd
+            } 
+            if(mm<10) {
+                mm='0'+mm
+            } 
+            today = yyyy+'-'+mm+'-'+dd;
+
+            if(date === undefined){
+                date = today;
+            }
+            if(time === undefined){
+                time = '12:00';
+            }
+            
+            console.log(reminder);
+            console.log(date);
+            console.log(time);
+
+            context.succeed(
+                generateResponse(
+                    buildSpeechletResponse('Reminder is ' + reminder + ', date is ' + date + ', and time is ' + time, true),
+                    {}
+                )
+            )
+
+
             break;
+
+         case "AMAZON.HelpIntent":
+      	    console.log('HELP REQUEST');
+      	    context.succeed(
+                generateResponse(
+                    buildSpeechletResponse('You can ask me to send a reminder to your Pebble Smartwatch with the reminder details, a date, and a time.... How may I help you?', false),
+                    {}
+                )
+            )
+        break;
+        
+        case "AMAZON.CancelIntent":
+      	    console.log('HELP REQUEST');
+      	    context.succeed(
+                generateResponse(
+                    buildSpeechletResponse('Thank you for using Pebble Push', true),
+                    {}
+                )
+            )
+        break;
+        
+        case "AMAZON.StopIntent":
+      	    console.log('HELP REQUEST');
+      	    context.succeed(
+                generateResponse(
+                    buildSpeechletResponse('Thank you for using Pebble Push', true),
+                    {}
+                )
+            )
+        break;
 
           default:
             throw "Invalid intent"
         }
 
         break;
+    
+    
 
       case "SessionEndedRequest":
         // Session Ended Request
-        console.log(`SESSION ENDED REQUEST`)
+        console.log('SESSION ENDED REQUEST')
         break;
 
       default:
-        context.fail(`INVALID REQUEST TYPE: ${event.request.type}`)
+        context.fail('INVALID REQUEST TYPE: ${event.request.type}')
 
     }
 
-  } catch(error) { context.fail(`Exception: ${error}`) }
+  } catch(error) { context.fail('Exception: '+error)}
 
 }
 
